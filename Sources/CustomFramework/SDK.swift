3f3e69e4636da59386ffc4dd8b8a726764398acc
Mayur Saitwal <57178570+tapmyads-dev1@users.noreply.github.com>
2022-09-16 16:33:15 +0530

    f

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  ToTestApi
+//
+//  Created by Developer1 on 16/09/22.
+//
+
+import Foundation
+
+struct SDK{
+    static func doSomeWork(){
+        print("Doing some work")
+    }
+}
