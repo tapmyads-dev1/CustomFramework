9e7dd92c849feee6a865303586ba5fe7683ab46b
Mayur Saitwal <57178570+tapmyads-dev1@users.noreply.github.com>
2022-09-16 16:51:48 +0530

    final

--- Package.swift
+++ Package.swift
@@ -21,16 +21,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.1"),
+       // .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CustomFramework",
-            dependencies: []),
-        .testTarget(
-            name: "CustomFrameworkTests",
-            dependencies: ["CustomFramework"]),
-    ]
+          // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+          // Targets can depend on other targets in this package, and on products in packages this package depends on.
+          .binaryTarget(
+              name: "CustomFramework",
+              
+              path: "./Sources/CustomFramework.xcframework")
+      ]
 )
